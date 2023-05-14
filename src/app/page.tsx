cdce164297f192f638895549aebf36fc61ca1990
Thomas DOUCHE <Thomas.douche@aol.com>
2023-05-14 20:16:56 +0200

    fixed key for div list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 
   const initialMessages = ["Hello", "My name is ChatKinsey", "I am here to help you."];
   const [messages, setMessages] = useState(initialMessages);
-  const messageItems = messages.map(message => <div>{message}</div>);
+  const messageItems = messages.map( (message, index) => <div key={index}>{message}</div>);
 
   function addItem(text: string) {
     setMessages([...messages, text]);
