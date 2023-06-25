2b7bb943f9633e6e75441d945d25073870d31575
Thomas D <33961528+Stentor88Ch216@users.noreply.github.com>
2023-06-25 23:32:20 +0200

    Delete src directory

--- src/app/api/myapi/route.ts
+++
@@ -1,8 +0,0 @@
-export const runtime = 'edge'
-
-
-export async function GET(req: Request): Promise<Response> {
-
-    return new Response("Hello world");
-};
-
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import "./styles.css"
-import MyPage from "./MyPage";
-
-export default async function Home() {
-
-  return (
-    <MyPage/>
-  );
-}
